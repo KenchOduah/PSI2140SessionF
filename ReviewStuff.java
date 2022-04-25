c1b24257d6673908b60e7f2132d974838522c46e
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-25 16:26:14 -0400

    Add files via upload

---
+++ ReviewStuff.java
@@ -0,0 +1,19 @@
+/* 1. Write a program that prompts the user for an integer and displays 
+ * if the provided integer is a prime number or not.
+   A prime number is a number that is divisible only by 1 and itself
+   2. Create a class that asks the user to enter a number. 
+   Call a method isEven that returns true or false if the number is even.
+   The return from isEven should be passed to printEven which will print
+   "The number is even" if the number is even and "The number is odd" if the number is odd.
+   Determination of what to print must be done based on the return from the isEven method.
+   3. Create a bike class that has wheels, a name, color, price, and purchase year(1996, 1997, etc)
+   If the bike was purchased before 2021 it should have old printed next to the year
+   IF the bike was purchased during 2021 it should have current printed next to the year
+   If the bike was purchased after 2021 it should have new printed next to the yeaar
+*/
+
+import java.util.Scanner;
+public class ReviewStuff {
+	public static void main(String[] args) {
+	}
+}
